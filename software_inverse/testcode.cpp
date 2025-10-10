0a1397bc4ba62b337e53a970ac81e183abb328da
AthiRamRS <143102635+ARX-0@users.noreply.github.com>
2025-10-10 14:58:06 +0530

    Final.

--- software_inverse/testcode.cpp
+++ software_inverse/testcode.cpp
@@ -1,122 +1,110 @@
 #include <iostream>
+#include "qbMatrix3.hpp"
 #include <iomanip>
-#include <string>
 #include <vector>
+#include <math.h>
+#include <string>
 #include <sstream>
 #include <fstream>
-#include "qbMatrix3.hpp"
 
 using namespace std;
 
-// ---------------- Print ---------------- //
+
+// *******************************************************/
 template<class T>
-void qbMatrix2<T>::PrintMatrix() {
-    for (int i = 0; i < m_nRows; i++) {
-        for (int j = 0; j < m_nCols; j++)
-            std::cout << std::setw(10) << m_matrix_Data[Sub2Ind(i, j)] << " ";
-        std::cout << "\n";
+void PrintMatrix(qbMatrix2<T> matrix) {
+int nRows = matrix.GetNumRows();
+int nCols = matrix.GetNumCols();
+
+for(int i=0;i<nRows;i++){
+    for(int j=0;j<nCols;j++){
+        cout<< std::fixed << std::setprecision(3) << matrix.GetElement(i,j)<<"  ";
+        }
+    cout<<endl;
     }
 }
-
+// *******************************************************/
 int main() {
-    std::string filename = "test_matrices.csv";
-    ifstream input(filename);
+    float tolerance = 1e-9;
+std::string filename = "test_matrix_OG.csv";
 
-    if (!input.is_open()) {
-        std::cerr << "Could not open file: " << filename << std::endl;
-        return 1;
+ifstream input(filename);
+
+if(input.is_open()){
+
+std::string currentline;
+getline(input,currentline);
+
+size_t delimiterPosition = currentline.find(" ");
+std::string  nRowsString = currentline.substr(delimiterPosition, currentline.length());
+
+int nRows = atoi(nRowsString.c_str());
+
+std::stringstream ss;
+double currentNumber;
+
+std::vector<double> currentRow;
+
+std::vector<double> currentMatrixData;
+
+std::vector<qbMatrix2<double> *> failedTests;
+
+int totalRows = 0;
+int rowCount = 0;
+int numSuccess = 0;
+int numFail = 0;
+
+while(!input.eof()){
+    getline(input,currentline);
+    ss<< currentline;
+
+    while(ss>>currentNumber){
+        currentRow.push_back(currentNumber);
+        if(ss.peek() == ',')
+            ss.ignore();
     }
+    ss.clear();
 
-    std::string currline;
-    getline(input, currline);
+    currentMatrixData.insert(currentMatrixData.end(), currentRow.begin(), currentRow.end());
+    currentRow.clear();
+    rowCount++;
 
-    // Header: number of rows
-    int nRows = 0;
-    size_t spacepos = currline.find(" ");
-    if (spacepos != std::string::npos) {
-        std::string nrows_str = currline.substr(spacepos);
-        nRows = atoi(nrows_str.c_str());
-    } else {
-        nRows = atoi(currline.c_str());
-    }
+    if(rowCount > 4){
+        rowCount = 0;
 
-    std::stringstream ss;
-    double currentNumber;
-    std::vector<double> currentRow;
-    std::vector<double> currentMatrixData;
-    std::vector<qbMatrix2<double>*> failedMatrices;
+        qbMatrix2<double> newMatrix(5,10,&currentMatrixData);
 
-    int rowCount = 0;
-    int numSuccess = 0;
-    int numFailures = 0;
-    const double tolerance = 1e-9;
+        qbMatrix2<double> leftMatrix;
+        qbMatrix2<double> rightMatrix;
 
-    // Read matrices row by row
-    while (getline(input, currline)) {
-        ss << currline;
-        while (ss >> currentNumber) {
-            currentRow.push_back(currentNumber);
-            if (ss.peek() == ',') ss.ignore();
+        newMatrix.Seperate(&leftMatrix,&rightMatrix,5);
+
+
+        leftMatrix.Inverse();
+
+        if(leftMatrix.Compare(rightMatrix,tolerance)){
+            cout << "invert test passed." << endl;
+            numSuccess++;
         }
-        ss.clear();
+        else{
+            cout << "invert test failed " << endl;
+            numFail++;
 
-        currentMatrixData.insert(currentMatrixData.end(), currentRow.begin(), currentRow.end());
-        currentRow.clear();
-        rowCount++;
-
-        if (rowCount == nRows) {
-            rowCount = 0;
-
-            if (currentMatrixData.size() == static_cast<size_t>(nRows * 2 * nRows)) {
-                // Build augmented matrix from the vector data
-                qbMatrix2<double> newMatrix(nRows, 2 * nRows, &currentMatrixData);
-
-                qbMatrix2<double> leftMatrix;
-                qbMatrix2<double> rightMatrix;
-                newMatrix.Seperate(&leftMatrix, &rightMatrix, nRows);
-
-                bool invertible = leftMatrix.Inverse();
-                if (invertible && leftMatrix.Compare(rightMatrix, tolerance)) {
-                    numSuccess++;
-                } else {
-                    numFailures++;
-                    failedMatrices.push_back(new qbMatrix2<double>(newMatrix));
-                }
-            } else {
-                std::cerr << "Malformed input: expected "
-                          << nRows * 2 * nRows << " elements, got "
-                          << currentMatrixData.size() << std::endl;
-            }
-            currentMatrixData.clear();
+            failedTests.push_back(new qbMatrix2<double>(newMatrix));
         }
+        currentMatrixData.clear();
     }
+    totalRows++;
+}
+cout << "Total tests: " << (numSuccess + numFail) << endl;
+cout << "Total passed: " << numSuccess << endl;
+cout << "Total failed: " << numFail << endl;
 
-    // Print summary in your requested format
-    std::cout << "\n====================================\n";
-    std::cout << "Number of compared Successes with the Golden output : " << numSuccess << std::endl;
-    std::cout << "Number of Failures with comparisons with the Golden output : " << numFailures << std::endl;
-    std::cout << "The Error tolerance range used : " << tolerance << std::endl;
-    std::cout << "====================================\n";
-
-    // Ask before printing failed matrices
-    if (numFailures > 0) {
-        char choice;
-        std::cout << "Do you want to print the failed matrices? (Y/N): ";
-        std::cin >> choice;
-
-        if (choice == 'Y' || choice == 'y') {
-            for (qbMatrix2<double>* currentMatrix : failedMatrices) {
-                std::cout << "---- Failed Matrix ----" << std::endl;
-                currentMatrix->PrintMatrix();
-                delete currentMatrix;
-            }
-        } else {
-            for (qbMatrix2<double>* currentMatrix : failedMatrices) {
-                delete currentMatrix;
-            }
-        }
-    }
-
-    input.close();
+//Print the matrices that caused the test to fail.
+for(qbMatrix2<double> *currentMatrix : failedTests){
+    PrintMatrix(*currentMatrix);
+    cout << "------------------" << endl;
+}
+}
     return 0;
 }
